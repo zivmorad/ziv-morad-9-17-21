98ff1620cd0a7ef01d5a1e3be3e8e360809f373f
Ziv Morad <zivm@rightcodelabs.com>
2021-09-19 23:25:16 +0300

    prod

--- src/axios/index.js
+++ src/axios/index.js
@@ -3,10 +3,10 @@ import axios from 'axios';
 export const accuweather = axios.create({
 	baseURL: 'https://dataservice.accuweather.com',
 	params: {
-		apikey: '0H4RNCzLTuBcXWBJNuKwN2yagWgDghN3',
+		apikey: '6cAQHMaddjbwcUNmq7eKD0zXBK216Lvn',
 	},
 });
-
+// 6cAQHMaddjbwcUNmq7eKD0zXBK216Lvn
 // v44g2voakObqbIJP3i3pSsoouLFXELxJ
 // kvSw3yjdeGvnXU76NhhxEt2YBovLGeLh
 //eLG4CrQCOGDxRAAe61oAK1v3FvlpPvfA
